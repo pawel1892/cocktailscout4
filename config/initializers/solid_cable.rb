9f561011b1dc0aa411a7ff4bddd2269a527922a5
Pawel <pawel@cofta.de>
2026-01-23 22:23:08 +0700

    fix deployment

--- config/initializers/solid_cable.rb
+++ config/initializers/solid_cable.rb
@@ -2,6 +2,8 @@
 # This runs after Rails is fully initialized
 Rails.application.configure do
   config.after_initialize do
-    SolidCable.connects_to = { database: { writing: :cable } } if defined?(SolidCable)
+    if defined?(SolidCable) && SolidCable.respond_to?(:connects_to=)
+      SolidCable.connects_to = { database: { writing: :cable } }
+    end
   end
 end
