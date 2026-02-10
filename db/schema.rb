526cb54ec1c280709b43d467e22c9e8f46e3ef2c
Pawel <pawel@cofta.de>
2026-02-10 15:02:34 +0700

    Update schema after public_id NOT NULL migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_10_030902) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_10_071004) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
